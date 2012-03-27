5ca8df3f14dd4c4c62be575d0456e1d5d90826cc
Libida <myr-kat@bk.ru>
2012-03-27 14:58:55 +0300

    Test commit

--- src/main/java/by/bsuir/tournament/validation/ParticipantValidator.java
+++ src/main/java/by/bsuir/tournament/validation/ParticipantValidator.java
@@ -1,7 +1,7 @@
 package by.bsuir.tournament.validation;
 
-import by.bsuir.tournament.model.Participant;
 import org.springframework.validation.Errors;
+import by.bsuir.tournament.model.Participant;
 
 public class ParticipantValidator extends PersonValidator {
   public void validate(Participant participant, Errors errors) {
